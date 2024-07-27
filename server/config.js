cdbfa8adc9b13a4178ceb0020cad21778dd562d1
antoni0neto <antonio.silva6@yahoo.com.br>
2024-07-27 08:36:43 -0300

    Final project

--- server/config.js
+++ server/config.js
@@ -2,14 +2,15 @@ var config = {
     dev: {
         url: 'http://localhost/',
         port: 3000,
-        ambiente: 'DEV', 
+        ambiente: 'DEV',
         database: {
-            host: '127.0.0.1',
+            host: 'localhost',
             port: 3306,
             user: 'root',
             password: 'root',
-            database: 'pizzaria'
-        }
+            database: 'delivery',
+            timezone: 'utc'
+        },
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -1,48 +1,50 @@
 global.config = require('./config').get('dev');
 
 const restify = require("restify");
-const path = require("path");
-const recursiveReaddir = require("recursive-readdir")
+const recursiveReaddir = require('recursive-readdir');
+const path = require('path');
 
-// Cria o servidor
+// Inicia o servidor
 const server = restify.createServer({
     name: 'Delivery',
     version: '1.0.0'
 });
 
-// Adiciona as extensões do restify para o funcionamento do JSON nas requsições
+// Adiciona as extensões do restify para o funcionamento do JSON nas requisições
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
-server.use(restify.plugins.urlEncodedBodyParser());
+server.use(restify.plugins.urlEncodedBodyParser())
 
-// Adiciona todas as Rotas dentro da inicialização do server (para escutar as rotas na pasta "routes")
+// Adiciona todas as Rotas dentro da inicialização do server (para escutar todas as rotas)
 const pathFiles = path.resolve(path.resolve('./').concat('/server/routes'));
 
 recursiveReaddir(pathFiles, ['!*.js'], (err, files) => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
+    if (err) { console.log(err); process.exit(1) }
     files.forEach(element => { require(element)(server) })
 });
 
-// Utilizado para não dar problema com requisições no Chrome (CORS)
+// Utilizado para nçao dar problema com requisições no Chrome (Cors)
 server.use(
     function nocache(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        res.header("Pragma", "no-cache");
+        res.header('Pragma', 'no-cache');
         next();
     }
-)
+);
 
-// Modifica o array de erro e mostra pro usuário uma mensagem personalizada
+// Cria a rota pública dentro do server para as imagens do cardapio
+server.get('/public/images/*', restify.plugins.serveStatic({
+    directory: __dirname,
+}))
+
+// Modifica o array de erro (página não encontrada)
 server.on('restifyError', function(req, res, err, callback) {
     err.toJSON = function customToJSON() {
         return {
             Erro: 'Página não encontrada :/'
-        }
+        };
     };
     return callback();
 });
